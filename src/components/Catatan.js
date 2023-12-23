8bc12e24d2ca9292ecf21e5623a91a02bbca00a9
M. Ramandha Kurniawan Batubara <103089301+Pressureds@users.noreply.github.com>
2023-12-24 00:32:01 +0800

    Create Catatan.js

---
+++ src/components/Catatan.js
@@ -0,0 +1,21 @@
+// Catatan.js
+import React from 'react';
+
+const Catatan = ({ catatan, onDelete }) => {
+  return (
+    <div className="card mb-3">
+      <div className="card-body">
+        <h5 className="card-title">{catatan.judul}</h5>
+        <p className="card-text">{catatan.isi}</p>
+        <p className="card-text">
+          <small className="text-muted">{catatan.waktu}</small>
+        </p>
+        <button onClick={onDelete} className="btn btn-danger ms-2">
+          Hapus
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Catatan;
